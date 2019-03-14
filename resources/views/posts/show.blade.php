9b3458f54e6879985b5d160eec1132401ea45035
EsraaKedwany <esraa.mohamed1006@gmail.com>
2019-03-14 17:16:40 +0200

    edit"show post"

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,17 +2,38 @@
 @section('content')
 
 <form>
-  <div class="form-group row">
-    <label for="staticEmail" class="col-sm-2 col-form-label">Title</label>
-    <div class="col-sm-10">
-      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="{{$post->title}}">
+  <h3>Post Info</h3>
+    <div class="form-group row">
+      <label for="staticEmail" class="col-sm-2 col-form-label">Title</label>
+      <div class="col-sm-10">
+        <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="{{$post->title}}">
+      </div>
     </div>
-  </div>
-  <div class="form-group row">
-    <label for="staticEmail" class="col-sm-2 col-form-label">Description</label>
-    <div class="col-sm-10">
-      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="{{$post->description}}">
+    <div class="form-group row">
+      <label for="staticEmail" class="col-sm-2 col-form-label">Description</label>
+      <div class="col-sm-10">
+        <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="{{$post->description}}">
+      </div>
     </div>
-  </div>
+
+  <h3>Post creator Info</h3>
+    <div class="form-group row">
+      <label for="staticEmail" class="col-sm-2 col-form-label">Name:</label>
+      <div class="col-sm-10">
+        <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="{{$post->user->name}}">
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="staticEmail" class="col-sm-2 col-form-label">Email:</label>
+      <div class="col-sm-10">
+        <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="{{$post->user->email}}">
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="staticEmail" class="col-sm-2 col-form-label">Created at:</label>
+      <div class="col-sm-10">
+        <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="{{$post->user->created_at}}">
+      </div>
+    </div>
 </form>
 @endsection
